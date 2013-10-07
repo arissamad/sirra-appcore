4f195545be92c0d87a29fd80b0553c2fc5134779
Aris Samad-Yahaya <aris@quickschools.com>
2013-10-06 22:45:08 -0700

    Can now recognize hibernate discriminator columns

--- src/main/java/com/sirra/appcore/sql/Columns.java
+++ src/main/java/com/sirra/appcore/sql/Columns.java
@@ -1,12 +1,18 @@
 package com.sirra.appcore.sql;
 
+import java.lang.annotation.*;
 import java.lang.reflect.*;
 import java.util.*;
 
+import javax.persistence.*;
+
 import org.hibernate.*;
 import org.hibernate.metadata.*;
+import org.hibernate.persister.entity.*;
+import org.reflections.*;
 
 import com.sirra.server.json.*;
+import com.sirra.server.rest.*;
 import com.sirra.server.session.*;
 
 /**
@@ -37,7 +43,12 @@ public class Columns {
 		columns.add(classMetadata.getIdentifierPropertyName());
 		for(String name: propertyNames) {
 			columns.add(name);
-		}	
+		}
+		
+		if(classMetadata instanceof SingleTableEntityPersister) {
+			SingleTableEntityPersister persister = (SingleTableEntityPersister) classMetadata;
+			columns.add(persister.getDiscriminatorColumnName());
+		}
 	}
 	
 	public void add(String col) {
@@ -73,19 +84,39 @@ public class Columns {
 		
 		try {
 			
+			DiscriminatorColumn discriminatorAnnotation = (DiscriminatorColumn) entityClass.getAnnotation(DiscriminatorColumn.class);
+			String discriminatorName = null;
+			Map<String, Class> subClassLookup = new HashMap();
+			
+			if(discriminatorAnnotation != null) {
+				discriminatorName = discriminatorAnnotation.name();
+				
+				Reflections reflections = new Reflections(ApiServlet.getAPIPackageBase());
+		    	Set classes = reflections.getSubTypesOf(entityClass);
+		    	
+		    	for(Object classObject: classes) {
+		    		Class clazz = (Class) classObject;
+		    		
+		    		DiscriminatorValue subDiscriminator = (DiscriminatorValue) clazz.getAnnotation(DiscriminatorValue.class);
+		    		
+		    		subClassLookup.put(subDiscriminator.value(), clazz);
+		    	}
+			}
+			
 			for(Data data: dataList) {
-				T object = (T) entityClass.newInstance();
+
+				T object;
+				
+				if(discriminatorName != null) {
+					Class subEntityClass = subClassLookup.get(data.get(discriminatorName));
+					object = (T) subEntityClass.newInstance();
+				} else {
+					object = (T) entityClass.newInstance();
+				}
 				
 				for(String fieldName: getColumns()) {
-					try {
-						Field field = entityClass.getDeclaredField(fieldName);
-						
-						Object value = data.get(fieldName);
-						field.setAccessible(true);
-						field.set(object, value);
-					} catch(Exception e) {
-						throw new RuntimeException(e);
-					}
+					Object value = data.get(fieldName);
+					Setter.set(object, fieldName, value);
 				}
 				
 				resultList.add(object);
